23de71738a2dd409051ea1a5ba9a06b775ee6ebd
Lucien Perret <lucienperret@icloud.com>
2024-06-16 14:49:03 +0200

    snp basic_syntax and modularization

---
+++ 2nd_Semester/SNP/example_code/02_modularisierung/modularization.c
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "modularization.h" //include none standard library header file
+
+int main(int argc, char *argv[])
+{
+	return EXIT_SUCCESS;
+}
